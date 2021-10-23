b7e3e4097bd0943ed74e2af4fa2a9f3a3c1c2fc7
Georgi Krastev <georgikrastev1@gmail.com>
2021-10-23 23:11:10 +0300

    Added data model

---
+++ HTMLPreviewWeb/Models/HTMLContent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HTMLPreviewWeb.Models
+{
+    public class HTMLContent
+    {
+        public int ID { get; set; }
+        public string Description { get; set; }
+        [Column("Content")]
+        public string Content { get; set; }
+    }
+}
